9ef1a0a6b7123a9b5b2145607aed406cc437b7ca
Nick Butler <nwmbutler@gmail.com>
2020-06-08 17:36:48 +0100

    Fizzbuzz complete with all tests

--- jasmine/jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
+++ jasmine/jasmine-standalone-3.5.0/spec/fizzBuzzSpec.js
@@ -5,7 +5,15 @@ describe("FizzBuzz", function(){
     fizzBuzz = new FizzBuzz();
   });
 
-  it("should return a integer", function() {
+  it("should return FizzBuzz", function() {
+    expect(fizzBuzz.spin(15)).toEqual("FizzBuzz");
+  });
+
+  it("should return Fizz", function() {
+    expect(fizzBuzz.spin(3)).toEqual("Fizz");
+  });
+
+  it("should return Buzz", function() {
     expect(fizzBuzz.spin(5)).toEqual("Buzz");
   });
 
--- jasmine/jasmine-standalone-3.5.0/src/fizzBuzz.js
+++ jasmine/jasmine-standalone-3.5.0/src/fizzBuzz.js
@@ -1,6 +1,14 @@
 class FizzBuzz {
 
-   spin = function(){
-    return "Buzz";
-  }
+   spin(number){
+     if (number % 15 == 0){
+      return "FizzBuzz";
+  }  else if (number % 3 == 0){
+      return "Fizz";
+  }  else if (number % 5 == 0){
+      return "Buzz";
+  }  else {
+    return number;
+}
+}
 }
